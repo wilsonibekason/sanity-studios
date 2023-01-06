e1521f9a40441b19084893846ce0790899f7cfce
Wilson Ibekason <wanbekgithub@gmail.com>
2023-01-06 11:24:03 +0100

    Add sanity ts first query

---
+++ schemas/example/example.ts
@@ -0,0 +1,20 @@
+
+
+export default {
+    name: 'category',
+    title: 'Category',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+      },
+    ],
+  }
+  
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,40 @@
-export const schemaTypes = []
+// import createSchema from 'part:@sanity/base/schema-creator'
+
+// // Then import schema types from any plugins that might expose them
+// import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+// // Then we give our schema to the builder and provide the result to Sanity
+// export default createSchema({
+//   // We name our schema
+//   name: 'default',
+//   // Then proceed to concatenate our document type
+//   // to the ones provided by any plugins that are installed
+//   types: schemaTypes.concat([
+//     // The following are document types which will appear
+//     // in the studio.
+  
+//     // When added to this list, object types can be used as
+//     // { type: 'typename' } in other document schemas
+//   ]),
+// }) 
+
+import { createSchema } from "sanity"
+
+import {s} from "sanity-typed-schema-builder"  
+
+const foo = s.document({
+  name: "foo",
+  fields: [
+    {
+      name: "foo",
+      type: s.string()
+    }
+  ]
+})
+
+export const schemaTypes = [foo.schema()]
+
+export default createSchema({
+  name: "default",
+  types: schemaTypes,
+})
